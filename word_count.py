e0da1993c4642aaccfcfdfb96900fba318784779
Hackbright Student <info@hackbrightacademy.com>
2014-10-06 15:51:46 -0700

    Got rid of punctuation in read text

--- word_count.py
+++ word_count.py
@@ -1,6 +1,6 @@
 from sys import argv
 
-# import string
+import string
 
 
 # import file, open file
@@ -14,16 +14,17 @@ word_count = {}
 # for loop by line: checking for words, adding to dictionary
 for line in text:
     line = line.rstrip().lower()
-    # for char in line:
-    #     if char in string.punctuation:
-    #         char = " "
+    for punc in string.punctuation:
+        line= line.replace(punc,"")
     words = line.split()
 
+
     for word in words:
-        if word not in word_count.items():
-            word_count[word] = 1
-        else:
+        if word in word_count.keys():
             word_count[word] += 1
+        else:
+            word_count[word] = 1
+
 
 print word_count
 #get the word, increment the instances of each word
