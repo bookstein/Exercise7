dbe21367ef1702082772263bc546817fb13a3df3
Hackbright Student <info@hackbrightacademy.com>
2014-10-07 13:00:38 -0700

    Clean up code, separate into functions

--- ex6_sortedbyrating.py
+++ ex6_sortedbyrating.py
@@ -1,24 +1,24 @@
-# Open text file of scores. Read in the data contained
-restaurant = {}
+def main():
+    restaurant = sort_file("scores.txt")
+    print_file(restaurant)
 
-ratings = open("scores.txt")
+def sort_file(filename):
 
+    # Open text file of scores. Read in the data contained
+    restaurant = {}
 
-# Slice data by colon to get name of restaurant and scores in a for loop line by line
-#for line in f
-
-for line in ratings:
-    line = line.rstrip()
-    rest_name, rest_score = line.split(':')
-    restaurant[rest_name] = int(rest_score)
+    ratings = open(filename)
 
 
-keys = restaurant.keys()
-keys.sort()
+    # Slice data by colon to get name of restaurant and scores in a for loop line by line
+    #for line in f
 
-#print keys
-# for key in keys:
-#     print "Restaurant is: %s, score is %d" % (key, restaurant[key])
+    for line in ratings:
+        line = line.rstrip()
+        rest_name, rest_score = line.split(':')
+        restaurant[rest_name] = int(rest_score)
+
+    return restaurant
 
 
 # get values out of dictionary and sort by rating, then alphabetical order
@@ -29,10 +29,13 @@ def sort_by_rating(dictionary):
         sorted_list.append((item[1], item[0]))
     return sorted_list
 
-# sorted_rest = sort_by_rating(restaurant)
 
 # print sorted_rest
+def print_file(restaurant):
+    for rating, rest in sorted(sort_by_rating(restaurant)):
+        # restaurant[rest] = rating
+        print "Restaurant %s is rated at %d" % (rest, int(rating))
 
-for rating, rest in sorted(sort_by_rating(restaurant)):
-    # restaurant[rest] = rating
-    print "Restaurant %s is rated at %d" % (rest, int(rating))
+
+if __name__ == "__main__":
+    main()
