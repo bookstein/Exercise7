7ae52238707598e9783e06e72edab3e9e5828a67
Hackbright Student <info@hackbrightacademy.com>
2014-10-06 17:54:37 -0700

    More sorting, don't understand why it works on word_count4.py

---
+++ word_count4.py
@@ -0,0 +1,36 @@
+from sys import argv
+
+import string
+
+script, filename = argv
+
+text = open(filename)
+
+word_count = {}
+
+# for loop by line: checking for words, adding to dictionary
+for line in text:
+    line = line.rstrip().lower()
+    for punc in string.punctuation:
+        line= line.replace(punc," ")
+    words = line.split()
+
+
+    for word in words:
+        if not word_count.get(word):
+            word_count[word] = 1
+        else:
+            word_count[word] += 1
+
+# print key and value pairs
+# keys_list = word_count.keys()
+# keys_list.sort(word_count, key = word_count.get, reverse = True)
+
+
+sorted_list = sorted(word_count.iteritems(), key = lambda item: (item[1], item[0]), reverse = False)
+
+#print sorted_list
+
+for item in sorted_list:
+    print "%s, %s" % (item[0], item[1])
+
