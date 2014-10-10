e4b7c001cfc5b4938a64926cdf0a0672c8406d96
emilyboo <ehbookstein@gmail.com>
2014-10-10 09:35:22 -0700

    Delete few lines of code in practice file, start over. Create read_file func that creates list of words from text

--- word_count_practice.py
+++ word_count_practice.py
@@ -1,18 +1,42 @@
 """ This is a practice redo of Exercise 7 from Monday 10/6.
-Working on it on Tuesday 10/7.
+Working on it on Friday 10/10.
 How long does it take me to complete the exercise?"""
 
-from sys import argv
 import string
+from sys import argv
+# open and read in file -- no need to do it line by line
+# lowercase, punctuation, line split
 
-script, filename = argv
-
-def count_words(filename):
+def read_file(filename):
 	f = open(filename)
+	text = f.read()
+	text = text.lower()
+	words_list = text.split()
+	# for word in words_list:
+	# 	for punc in string.punctuation:
+	# 		if punc in word:
+				# //remove punctuation
 
-	for line in f:
-		line = line.rstrip()
-		textstring = line.split()
-		if string.punctuation in textstring:
-			# remove punctuation
+	print words_list
 
+
+
+
+# create dictionary of all words
+
+
+
+# loop through dictionary and count
+
+
+# main
+def main():
+	script, filename = argv
+	read_file(filename)
+	# make_dict()
+	# count_words()
+
+
+
+if __name__ == "__main__":
+	main()
