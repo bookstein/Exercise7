c48f3b97d141e68678e286b379681444affabb69
emilyboo <ehbookstein@gmail.com>
2014-10-11 22:02:15 -0700

    Redo of Ex7. Took 15 minutes to do basics (excluding punctuation and sorting and all extra credit.)

--- word_count_practice.py
+++ word_count_practice.py
@@ -7,14 +7,30 @@ from sys import argv
 # open and read in file -- no need to do it line by line
 # lowercase, punctuation, line split
 
+def read_file(filename):
+
+	dictionary = {}
+
+	f = open(filename)
+	text = f.read()
+	words = text.split()
+
+	for word in words:
+		dictionary[word] = dictionary.get(word, 0) + 1 # increments value by 1
+
+	print dictionary
+
+
+def sort_by_frequency(dict):
+	pass
+
 
 
 # main
 def main():
 	script, filename = argv
 	list_of_text = read_file(filename)
-	make_dict(list_of_text)
-	# count_words()
+
 
 
 
